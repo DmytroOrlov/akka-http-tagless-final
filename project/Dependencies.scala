a03075940e488972899a7543ec4769fbc2ebf066
Dmytro Orlov <orlov.dm@gmail.com>
2019-07-04 15:20:50 +0200

    build: update scalatest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
 }
